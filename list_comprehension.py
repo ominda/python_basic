9dd97d936c9ab5eb95917449d4090c6fd1bc3bca
Ominda Rathnasiri <ominda.rathnasiri@dialog.lk>
2023-09-07 17:11:27 +0530

    adding list comprehension knowledge

---
+++ list_comprehension.py
@@ -0,0 +1,15 @@
+# With this program, we can generate list from a given numbered list.
+# The generated list can be any type.
+# Much powerfull feature and we able to optimize our code with list comprehension.
+
+number_list = [911, 703, 892, 572, 164, 798, 989, 408]
+odd_even_list = []
+
+def is_odd(number: int) -> str:
+    return "Odd" if number % 2 != 0 else "Even"
+
+
+odd_even_list = [{number: is_odd(number)} for number in number_list]
+
+print(odd_even_list)
+
