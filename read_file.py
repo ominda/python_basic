b867bd1fef90345485896e17d6e0629a9923be0a
Ominda Rathnasiri <ominda.rathnasiri@dialog.lk>
2023-10-09 09:53:19 +0530

    add few files to repo

--- read_file.py
+++ read_file.py
@@ -2,9 +2,10 @@ with open('m.txt', 'r') as file:
     for line in file:
         # print(line.strip().split(','))
         l = tuple(line.strip().split(','))
-        print(type(l))
-        p = [ (name) for name in l ]
-        print(p)
+        print(type(l), l)
+        stu = [(subject, marks) for name, subject, marks in l]
+        print(stu)
+       
         # Ominda = [subject for name, subject in line.strip().split(',') if name == 'Ominda']
         # for i in line.strip().split(','):
         #     print(type(i), i)
---
+++ temp.py
@@ -0,0 +1,3 @@
+l = ('Ominda', 'Maths', 75)
+for name, sub, marks in l:
+    print(f'Name is {name}, Subject is {sub}, Marks: {marks}')
