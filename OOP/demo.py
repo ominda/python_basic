aeb5459f8430ce5adfedae0139eebd9d6987aa78
Ominda <SandaruwanRathnasiri@gmail.com>
2023-11-04 23:02:06 +0530

    update demo file

--- OOP/demo.py
+++ OOP/demo.py
@@ -1,17 +1,34 @@
 class Dog:
-    name = "x"
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
 
-    def walk(self):
-        print(type(self), self)
-        print("walking!")
-        # return True
+    def bark(self, color):
+        self.color = color
+        msg = f"I'm {self.name}, Woof Woof!!. I have beautiful {self.color}"
+        return msg
+    
+    def run(self):
+        msg = f"I'm {self.name}. I'm {self.age} old and I can run fast"
+        return msg
 
-dog1 = Dog()
-# dog1.name = "Pinky"
+dog1 = Dog("Browney", 4)
+print(dog1.bark("Black"))
 
-dog2 = Dog()
-# dog2.name = "Rexy"
+dog2 = Dog("Pinkey", 5)
+print(dog2.run())
 
-print(dog1.walk())
-print(dog1)
-# print(dog2.name)
+#####
+
+# class Dog:
+#     def __init__(self) -> None:
+#         print("Hello")
+
+
+# dog1 = Dog()
+# dog2 = Dog()
+
+#####
+
+# class MyClass:
+#     pass
