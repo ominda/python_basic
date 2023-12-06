444050419062848679ff13eab23a3c2f7dd3d5c5
Ominda <SandaruwanRathnasiri@gmail.com>
2023-12-06 22:32:34 +0530

    update read file

--- read_file.py
+++ read_file.py
@@ -1,14 +1,21 @@
+marks_dict = {}
+student_list = []
+
 with open('m.txt', 'r') as file:
     for line in file:
         # print(line.strip().split(','))
         l = tuple(line.strip().split(','))
-        print(type(l), l)
-        stu = [(subject, marks) for name, subject, marks in l]
-        print(stu)
+        # print(type(l), l)
+        name, subject, marks = l
+
+
+        # stu = [(subject, marks) for name, subject, marks in l]
+        # print(stu)
        
         # Ominda = [subject for name, subject in line.strip().split(',') if name == 'Ominda']
         # for i in line.strip().split(','):
         #     print(type(i), i)
 
 # print(Ominda)
+print(marks_dict.values())
 
